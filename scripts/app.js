898dec434e755989c98cb40962c692af65faf72f
Ada <ada_nowak@icloud.com>
2022-12-04 09:53:27 +0100

    console version of rps finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,57 @@
+let getUserChoice;
+let computerSelection;
+let playerSelection;
+let userPointsCounter = 0;
+let computerPointsCounter = 0;
+
 function getComputerChoice(){
     const options = ['rock', 'paper', 'scissors'];
     const selectRandom = Math.floor(Math.random() * options.length);
     return options[selectRandom];
 };
 
-let getUserChoice;
-let computerSelection;
-let playerSelection;
-
 function playRound(playerSelection, computerSelection){
     getUserChoice = prompt('Rock, paper, scissors! Choose one: ');
     playerSelection = getUserChoice.toLowerCase();
     computerSelection = getComputerChoice();
     if(playerSelection == computerSelection){
+        console.log('Tie in this round!')
         return `It's a tie!`;
     }else if(playerSelection == 'paper' && computerSelection == 'scissors'){
-        return `Computer won! Scissors beats paper`;
+        console.log(`Computer won! Scissors beats paper`);
+        return computerPointsCounter++;
     }else if(playerSelection == 'scissors' && computerSelection == 'rock'){
-        return `Computer won! Rock beats scissors`;
+        console.log(`Computer won! Rock beats scissors`);
+        return computerPointsCounter++;
     }else if(playerSelection == 'rock' && computerSelection == 'paper'){
-        return `Computer won! Paper beats rock`;
+        console.log(`Computer won! Paper beats rock`);
+        return computerPointsCounter++;
     }else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        return `You won! Scissors beats paper`;
+        console.log(`You won! Scissors beats paper`);
+        return userPointsCounter++;
     }else if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        return `You won! Rock beats scissors`;
+        console.log(`You won! Rock beats scissors`);
+        return userPointsCounter++;
     }else if(playerSelection == 'paper' && computerSelection == 'rock'){
-        return `You won! Paper beats rock`;
+        console.log(`You won! Paper beats rock`);
+        return userPointsCounter++;
     }else{
         return `Your choice is incorrect! Select rock, paper or scissors!`
     }
+};
+
+function game(){
+ 
+    for(let rounds = 0; rounds < 5; rounds++){
+        playRound();
+    }
+    if(computerPointsCounter > userPointsCounter){
+        return(`Five rounds are over! Computer won ${computerPointsCounter} to ${userPointsCounter}`);
+    }else{
+        return(`Five rounds are over! You won ${userPointsCounter} to ${computerPointsCounter}`);
+    }
+    
 }
 
-
+console.log(game());
 
