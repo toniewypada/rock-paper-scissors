23625cb995661395e3b1354267ff8363994ea884
Ada <ada_nowak@icloud.com>
2022-12-05 21:27:17 +0100

    started working on UI

--- scripts/app.js
+++ scripts/app.js
@@ -4,54 +4,54 @@ let playerSelection;
 let userPointsCounter = 0;
 let computerPointsCounter = 0;
 
-function getComputerChoice(){
-    const options = ['rock', 'paper', 'scissors'];
-    const selectRandom = Math.floor(Math.random() * options.length);
-    return options[selectRandom];
-};
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
 
-function playRound(playerSelection, computerSelection){
-    getUserChoice = prompt('Rock, paper, scissors! Choose one: ');
-    playerSelection = getUserChoice.toLowerCase();
-    computerSelection = getComputerChoice();
-    if(playerSelection == computerSelection){
-        console.log('Tie in this round!')
-        return `It's a tie!`;
-    }else if(playerSelection == 'paper' && computerSelection == 'scissors'){
-        console.log(`Computer won! Scissors beats paper`);
-        return computerPointsCounter++;
-    }else if(playerSelection == 'scissors' && computerSelection == 'rock'){
-        console.log(`Computer won! Rock beats scissors`);
-        return computerPointsCounter++;
-    }else if(playerSelection == 'rock' && computerSelection == 'paper'){
-        console.log(`Computer won! Paper beats rock`);
-        return computerPointsCounter++;
-    }else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        console.log(`You won! Scissors beats paper`);
-        return userPointsCounter++;
-    }else if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        console.log(`You won! Rock beats scissors`);
-        return userPointsCounter++;
-    }else if(playerSelection == 'paper' && computerSelection == 'rock'){
-        console.log(`You won! Paper beats rock`);
-        return userPointsCounter++;
-    }else{
-        return `Your choice is incorrect! Select rock, paper or scissors!`
-    }
-};
-
-function game(){
- 
-    for(let rounds = 0; rounds < 5; rounds++){
-        playRound();
-    }
-    if(computerPointsCounter > userPointsCounter){
-        return(`Five rounds are over! Computer won ${computerPointsCounter} to ${userPointsCounter}`);
-    }else{
-        return(`Five rounds are over! You won ${userPointsCounter} to ${computerPointsCounter}`);
-    }
-    
+function getComputerChoice() {
+  const options = ["rock", "paper", "scissors"];
+  const selectRandom = Math.floor(Math.random() * options.length);
+  return options[selectRandom];
 }
 
-console.log(game());
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getUserChoice();
+  computerSelection = getComputerChoice();
+  if (playerSelection == computerSelection) {
+    console.log("Tie in this round!");
+    return `It's a tie!`;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    console.log(`Computer won! Scissors beats paper`);
+    return computerPointsCounter++;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    console.log(`Computer won! Rock beats scissors`);
+    return computerPointsCounter++;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    console.log(`Computer won! Paper beats rock`);
+    return computerPointsCounter++;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    console.log(`You won! Scissors beats paper`);
+    return userPointsCounter++;
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    console.log(`You won! Rock beats scissors`);
+    return userPointsCounter++;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log(`You won! Paper beats rock`);
+    return userPointsCounter++;
+  } else {
+    return `Your choice is incorrect! Select rock, paper or scissors!`;
+  }
+}
 
+function game() {
+  // for(let rounds = 0; rounds < 5; rounds++){
+  //     playRound();
+  // }
+  if (computerPointsCounter > userPointsCounter) {
+    return `Five rounds are over! Computer won ${computerPointsCounter} to ${userPointsCounter}`;
+  } else {
+    return `Five rounds are over! You won ${userPointsCounter} to ${computerPointsCounter}`;
+  }
+}
+
+// console.log(game());
