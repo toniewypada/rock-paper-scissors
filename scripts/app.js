e93da5af2f83fc7859498dd2cb7fd1b582d4d913
Ada <ada_nowak@icloud.com>
2022-12-06 17:18:05 +0100

    updated ui

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,8 @@ let computerSelection;
 let playerSelection;
 let userPointsCounter = 0;
 let computerPointsCounter = 0;
-
-const btnRock = document.querySelector(".rock");
-const btnPaper = document.querySelector(".paper");
-const btnScissors = document.querySelector(".scissors");
+let userChoiceBtns = document.querySelectorAll(".btn-move-select");
+let currentRoundInfo = document.querySelector(".current-round");
 
 function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
@@ -14,39 +12,38 @@ function getComputerChoice() {
   return options[selectRandom];
 }
 
+userChoiceBtns.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    playRound(choice.value, getComputerChoice());
+  });
+});
+
 function playRound(playerSelection, computerSelection) {
-  playerSelection = getUserChoice();
-  computerSelection = getComputerChoice();
   if (playerSelection == computerSelection) {
-    console.log("Tie in this round!");
+    currentRoundInfo.textContent = "Tie in this round!";
     return `It's a tie!`;
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log(`Computer won! Scissors beats paper`);
+    currentRoundInfo.textContent = `Computer won! Scissors beats paper`;
     return computerPointsCounter++;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log(`Computer won! Rock beats scissors`);
+    currentRoundInfo.textContent = `Computer won! Rock beats scissors`;
     return computerPointsCounter++;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log(`Computer won! Paper beats rock`);
+    currentRoundInfo.textContent = `Computer won! Paper beats rock`;
     return computerPointsCounter++;
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log(`You won! Scissors beats paper`);
+    currentRoundInfo.textContent = `You won! Scissors beats paper`;
     return userPointsCounter++;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log(`You won! Rock beats scissors`);
+    currentRoundInfo.textContent = `You won! Rock beats scissors`;
     return userPointsCounter++;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log(`You won! Paper beats rock`);
+    currentRoundInfo.textContent = `You won! Paper beats rock`;
     return userPointsCounter++;
-  } else {
-    return `Your choice is incorrect! Select rock, paper or scissors!`;
-  }
+  } 
 }
 
 function game() {
-  // for(let rounds = 0; rounds < 5; rounds++){
-  //     playRound();
-  // }
   if (computerPointsCounter > userPointsCounter) {
     return `Five rounds are over! Computer won ${computerPointsCounter} to ${userPointsCounter}`;
   } else {
